8462fd1fd107aacbebbbc2f155cf53e9c56b26fe
menithanagesh <menithaacharya@gmail.com>
2026-07-18 13:25:29 +0530

    Time: 0 ms (100%), Space: 11.3 MB (9.93%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.c
@@ -0,0 +1,31 @@
+int value(char c){
+    switch(c){
+        case 'I': return 1;
+        case 'V': return 5;
+        case 'X': return 10;
+        case 'L': return 50;
+        case 'C': return 100;
+        case 'D': return 500;
+        case 'M': return 1000;
+        
+    }
+    return 0;
+}
+int romanToInt(char* s) {
+   
+   int num=0;
+   int i=0;
+   int n = strlen(s);
+   while(i<n){
+    if(value(s[i])<value(s[i+1])){
+        num -= value(s[i]);
+        i++;
+    }
+    else{
+        num += value(s[i]);
+        i++;
+    }
+   }
+   return num;
+  
+}
